3cbd26cc1a97c511f5a26e13d83301d489429901
Lawrence Kesteloot <lk@teamten.com>
2014-05-12 21:07:41 -0700

    Add navigation bar and analytics.

--- parseMarkdown.py
+++ parseMarkdown.py
@@ -18,10 +18,23 @@ header = """<!DOCTYPE html>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body id="help_page">
+        <div role="navigation" class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="/">designthology</a>
+            </div>
+        </div>
          <div class="container">
 """
 
 footer = """        </div>
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+          ga('create', 'UA-50725304-1', 'designthology.com');
+          ga('send', 'pageview');
+        </script>
     </body>
 </html>"""
 
