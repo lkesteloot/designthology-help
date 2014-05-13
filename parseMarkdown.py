f8276db65d2abbcb80f19ddf9fa3c7d15768c2bf
Lawrence Kesteloot <lk@teamten.com>
2014-05-12 17:08:32 -0700

    Accept command-line arguments.

--- parseMarkdown.py
+++ parseMarkdown.py
@@ -1,45 +1,46 @@
-#install using easy-install
+#install using pip or easy_install:
 #$ pip install markdown
 #current version 2.4
 import markdown
 import os
 import codecs
+import sys
 
-header = """<head>
+header = """<!DOCTYPE html><html><head>
         <title>designthology</title>
         <link rel="stylesheet" type="text/css" href="/static/css/home.css">
         <link rel="shortcut icon" href="/static/favicon.ico?fd00191b7d" type="image/x-icon">
         <link rel="icon" href="/static/favicon.ico?fd00191b7d" type="image/x-icon">
-    </head>"""
+    </head><body>"""
 
-footer = """<footer>
-            <h2>Designthology</h2>
-            <p>&copy; 2013</p>
-            <a href="mailto:jennifer@designthology.com">Contact Us</a>
-        </footer>"""
+footer = """</body></html>"""
 
 def createHTMLHeirarchy(init, destination):
     for root, dirs, files in os.walk(init):
         for name in files:
-            split = os.path.splitext(name)
-            if split[1] == ".md":
-                createHTMLFile(root, split[0], destination)
+            basename, extension = os.path.splitext(name)
+            if extension == ".md":
+                createHTMLFile(root, basename, destination)
 
 def parseMarkdown(file):
     text = codecs.open(file, 'r', encoding = 'utf8')
-    return markdown.markdown(text.read())
+    return markdown.markdown(text.read(), ["toc", "smarty"])
 
 #Takes a markdown file and 
 #creates a designthology html file.
 def createHTMLFile(root, baseName, destination):
-    if not os.path.exists(destination + root):
-        os.makedirs(destination + root)
+    pathname = os.path.join(destination, root)
+    if not os.path.exists(pathname):
+        os.makedirs(pathname)
 
-    html = parseMarkdown(root + baseName + ".md")
-    #print(html)
-    file = open(destination + root + baseName + ".html", 'w')
+    input_pathname = os.path.join(root, baseName + ".md")
+    output_pathname = os.path.join(destination, root, baseName + ".html")
+
+    html = parseMarkdown(input_pathname)
+    file = open(output_pathname, 'w')
     file.write(header)
     file.write(html)
     file.write(footer)
+    file.close()
 
-createHTMLHeirarchy("./", "./new/")
+createHTMLHeirarchy(sys.argv[1], sys.argv[2])
